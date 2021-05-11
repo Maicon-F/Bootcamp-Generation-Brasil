ee6fbf12fb56c03c0629e8bedffb0a6f0a7a5b51
Maicon Fernandes <76924074+Maicon-F@users.noreply.github.com>
2021-05-11 17:37:19 -0300

    Criado meu primeiro teste unitário
    Essa API possui como objetivo a introdução do usuário aos testes unitários.

---
+++ BLOCO II/TddWithJUnit-Spring/junit2/src/test/java/com/generation/Junit2/Junit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.Junit2;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Junit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
